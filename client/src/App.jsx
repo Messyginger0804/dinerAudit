23f4089830c624dee2fa5d1dca1273ca0c37ee6c
JC <j.c.ashley4363@gmail.com>
2023-09-17 22:00:16 -0500

    add context and context provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,21 +3,24 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UpdatePage from './pages/UpdatePage';
 import DetailPage from './pages/DetailPage';
 import Home from './pages/Home';
+import { RestaurantsContextProvider } from './context/RestaurantsConext';
 
 
 function App() {
   return (
-    <main>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" Component={Home} />
+    <RestaurantsContextProvider>
+      <main>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" Component={Home} />
 
-          <Route path='/restaurants/:id/update' Component={UpdatePage} />
+            <Route path='/restaurants/:id/update' Component={UpdatePage} />
 
-          <Route path='/restaurants/:id' Component={DetailPage} />
-        </Routes>
-      </BrowserRouter>
-    </main>
+            <Route path='/restaurants/:id' Component={DetailPage} />
+          </Routes>
+        </BrowserRouter>
+      </main>
+    </RestaurantsContextProvider>
   )
 }
 
