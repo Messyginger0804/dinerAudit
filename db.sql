d5d4b42fbdcc139a0f128969c44b40303009684f
JC <j.c.ashley4363@gmail.com>
2023-09-21 21:59:35 -0500

    got reviews page to render and created reviews table

--- db.sql
+++ db.sql
@@ -1,11 +1,12 @@
 -- commands
 
 
-CREATE TABLE restaurant (
+CREATE TABLE reviews (
     id BIGSERIAL NOT NULL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    location VARCHAR(255) NOT NULL,
-    price_range INT NOT NULL check (price_range >=1 and price_range <=5)
+    restaurant_id BIGINT NOT NULL REFERENCES restaurant(id),
+    name VARCHAR(55) NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL check (rating >=1 and rating <=5)
 );
 
 ALTER TABLE restaurant
